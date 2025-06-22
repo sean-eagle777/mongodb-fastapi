cc22b9c2e327a0e1aebc18bff1b265b5acf21067
sean-eagle777 <seanhsieh2014@gmail.com>
2025-06-22 21:30:27 +0800

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URL = "mongodb+srv://seanhsieh2014:5180551805Sean@cluster0.xfv28db.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URL = "mongodb+srv://seanhsieh201:5180551805Sean@cluster0.xfv28db.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 client = AsyncIOMotorClient(MONGO_URL)
